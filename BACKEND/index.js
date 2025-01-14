e7157ac9698969353c48743039e82ca1f52f6384
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2025-01-14 17:55:55 +0530

    CONNET MONGODB TO BACKEND

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT ||3000;
+const port = process.env.PORT || 3000;
+
+console.log(process.env);
+
+// connecting mongodb to node app
+// mongoose.connect() takes 2 argument 1.from which db to connect(db ke url se) 2. connection options
+
+
+mongoose.connect(process.env.MONGO_URL)
+    .then((x) => {
+        console.log("Connected to MongoDB!");
+    })
+    .catch((err) => {
+        console.error("Error while connecting to MongoDB:", err);
+    });
+    
+
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
