60a0581810179e235e9de1225606341b641e07c2
Arjun Raghav <125649858+Arjun9603@users.noreply.github.com>
2025-01-19 18:03:51 +0530

    Delete utils directory

--- utils/helper.js
+++
@@ -1,9 +0,0 @@
-const jwt=require('jsonwebtoken');
-exports={}
-
-exports.getToken=async (email,user)=>{
-        const token=jwt.sign({identifier: user._id},"ThiskeyisSecretOrPrivate");
-        return token;
-};
-
-module.exports=exports.getToken;
